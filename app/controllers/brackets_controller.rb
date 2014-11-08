364b49b828f777ebbfafe6e15d83be953363ee9b
Angelo Cordon <angelocordon@gmail.com>
2014-11-08 14:14:46 -0800

    Built match_init methods

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -5,25 +5,14 @@ class BracketsController < ApplicationController
 	end
 
 	def show
+		@match = Match.where(:bracket_id => @bracket)
 	end
 
-	def matchinit
-		# Tournament.id:38
-		# Bracket.id: 5
-		# @players = list of players [3,4,5,6,7,8]
-
-		@total = @activities.count/2  # = 3
-
-		@total.times do # Build Match 3 times
-			@match = Match.create(:tournament_id => @tournament, :player_ids => [])
-		end
-	end
-
+	
 	private
 		def set_brackets
 			@tournament = Tournament.find(params[:tournament_id])
 			@bracket = Bracket.find_by(:tournament_id => @tournament)
 			@activities = Activity.where(:tournament_id => @tournament)
 		end
-
 end
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,3 +1,41 @@
 class MatchesController < ApplicationController
+	before_action :set_matches
 	
+	def index
+	end
+
+	def show
+	end
+
+
+	def create
+		@total = @activities.count/2 
+		@players = Array.new
+		@gamer = Array.new
+
+		@activities.each do |activity|
+			@players << activity.player_id
+		end
+
+		@total.times do
+			@match = @bracket.matches.build
+				@gamers = @players.sample(2)
+				@match.player_ids = @gamers
+			@match.save
+			@gamers.each do |gamer|
+				@players.delete(gamer)
+			end
+		end
+	end	
+
+	def update
+	end
+
+
+	private
+		def set_matches
+			@tournament = Tournament.find(params[:tournament_id])
+			@bracket = Bracket.find_by(:tournament_id => @tournament)
+			@activities = Activity.where(:tournament_id => @tournament)
+		end
 end
