8594f77841b2e48c48fd1420cc652146f5c3a0a5
Angelo Cordon <angelocordon@gmail.com>
2014-12-13 07:37:10 -0800

    Worked on declerations

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,14 +5,7 @@ class MatchesController < ApplicationController
 	end
 
 	def show
-		# @match = Match.find(params[:id])
-		# @first = @match.player_ids.first
-		# @second = @match.player_ids.last
-		# @playerone = Player.find_by(id:@first)
-		# @playertwo = Player.find_by(id:@second)
-		# @tournament = @match.bracket.tournament
 		@match = Match.where(bracket_id:@bracket)
-		# @bracket = @match.bracket
 	end
 
 	def create
