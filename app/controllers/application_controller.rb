bbcd3cc5880a8b6e0df74d94243687ce9675b715
Angelo Cordon <angelocordon@gmail.com>
2014-12-15 06:41:43 -0800

    Displaying avatars in profiles and tournaments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :evetag
-  end
 
+	def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :evetag) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -15,7 +15,7 @@ class Player < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 
-  has_attached_file :avatar, :styles => { :medium => "165x165>", :thumb => "100x100#" }, :default_url => "/assets/medium/default.png"
+  has_attached_file :avatar, :styles => { :medium => "165x165#", :thumb => "100x100#" }, :default_url => "/assets/medium/default.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def match
